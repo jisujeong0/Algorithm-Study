ac02eaaf65541c70b955cadf21dd17485a94a057
jisu <98259433+jisujeong0@users.noreply.github.com>
2024-07-22 00:30:54 +0900

    [Silver III] Title: 반전 요세푸스, Time: 556 ms, Memory: 297788 KB -BaekjoonHub

---
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.valueOf(st.nextToken());
+        int k = Integer.valueOf(st.nextToken());
+        int m = Integer.valueOf(st.nextToken());
+        Deque<Integer> q = new ArrayDeque<Integer>();
+
+        for(int i=1; i<=n; i++) {
+            q.add(i);    
+        }
+
+        int count = 0;
+        int directionCount = 0;
+        boolean isLeftToRight = true;
+
+        while (!q.isEmpty()) {
+            int number;
+            if (isLeftToRight) {
+                number = q.pollFirst();
+            } else {
+                number = q.pollLast();
+            }
+
+            count += 1;
+            if (count == k) {
+                count = 0;
+                directionCount += 1;
+                sb.append(number).append("\n");
+            } else {
+                if(isLeftToRight) {
+                    q.addLast(number);
+                } else {
+                    q.addFirst(number);
+                }
+            }
+
+            if (directionCount == m) {
+                directionCount = 0;
+                isLeftToRight = !isLeftToRight;
+            }
+        }
+        System.out.println(sb);
+    }
+}
