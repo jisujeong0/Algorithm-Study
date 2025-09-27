481512b3742a7139a541cc78cae355d36b8c8d58
jisu <98259433+jisujeong0@users.noreply.github.com>
2025-09-27 17:11:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.63 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,29 +1,27 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        // 중복이면 value 값 +1
-        // value값만큼 key출력
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> player = new HashMap<>();
+        for (String p : participant) {
+            // player에 p가 있으면 +1
+            player.put(p, player.getOrDefault(p, 0) + 1);
+        }
+        for (String c : completion) {
+            // player에 c가 있으면 -1
+            player.put(c, player.get(c) - 1);
+        }
         
-        for (String player : participant) {
-            map.put(player, map.getOrDefault(player, 0) + 1);    
-        }
-        for (String player : completion) {
-            map.put(player, map.get(player) - 1);    
-        }
-
-        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
-
-        while(iter.hasNext()){
-            Map.Entry<String, Integer> entry = iter.next();
-            if (entry.getValue() != 0){
-                answer = entry.getKey();
+        String answer = "";
+        // 해시 순회하면서 value가 1 이상인 사람 출력
+        // 제한사항: completion의 길이는 participant의 길이보다 1 작습니다.
+        // 따라서 미완주자는 무조건 1명
+        for (String p : player.keySet()) {
+            if (player.get(p) > 0) {
+                answer = p;
                 break;
             }
         }
-        return answer;
+        return answer;    
     }
 }
