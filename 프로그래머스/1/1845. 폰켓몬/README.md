8c867a290362e4262a8cf6c69af871b59babdb3f
jisu <98259433+jisujeong0@users.noreply.github.com>
2025-09-29 23:59:20 +0900

    [level 1] Title: 폰켓몬, Time: 4.35 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 81.8 MB, 시간: 4.08 ms
+메모리: 88.2 MB, 시간: 4.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 22:56:34
+2025년 09월 29일 23:59:18
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,16 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> pocket = new HashSet<>();
-		for (int i=0; i<nums.length; i++) {
-			pocket.add(nums[i]);
-		}
-		
-		int answer = 0;
-		if (pocket.size() > nums.length/2) {
-			answer = nums.length/2;
-		} else {
-			answer = pocket.size();
-		}
-        return answer;
+        Set<Integer> pockets = new HashSet<>();
+        for (int n : nums) {
+            pockets.add(n);
+        }
+        int len = nums.length/2;
+        if (pockets.size() >= len) {
+            return len;
+        } else {
+            return pockets.size();
+        }
     }
 }
