4b90c77102de2c2562204e3ed0373dbd62698719
jisu <98259433+jisujeong0@users.noreply.github.com>
2025-11-15 00:51:54 +0900

    [Silver III] Title: N과 M (1), Time: 1992 ms, Memory: 76732 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        arr = new int[m];
+        visited = new boolean[n + 1];
+        backtracking(0);
+    }
+
+    private static void backtracking(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                backtracking(depth + 1);
+                visited[i] = false; 
+            }
+        }
+    }
+}
